7e54f4f30a0658c30e571a9afbbff0a41a016bad
LucienRRC <llin67@rrc.ca>
2026-06-17 17:55:27 -0500

    Add customers index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :customers, only: :index
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: redirect("/admin")
